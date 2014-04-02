d50793242730e974344e94946dc61e2a7f846d37
Snorri Páll Haraldsson <snorri@snorripall.com>
2014-04-02 22:24:18 +0000

    adding missing columns

--- db/migrate/20140330145133_create_blogs.rb
+++ db/migrate/20140330145133_create_blogs.rb
@@ -1,9 +1,10 @@
-class CreateBlogs< ActiveRecord::Migration
+class CreateBlogs < ActiveRecord::Migration
   def change
     create_table :spree_blogs do |t|
       t.string :title
       t.text :summary
-      t.string :link
+      t.text :body
+      t.string :permalink
       t.boolean :published
 
       t.timestamps
