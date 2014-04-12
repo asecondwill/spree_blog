18e325a6ff9d6f8f994b608644a4c1ff238f7918
Snorri Páll Haraldsson <snorri@snorripall.com>
2014-04-12 15:16:31 +0000

    Update for spree 2.2

--- lib/generators/spree_blog/install/install_generator.rb
+++ lib/generators/spree_blog/install/install_generator.rb
@@ -1,19 +1,20 @@
-module SpreeBlog
+  module SpreeBlog
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        # append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_blog\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_blog\n"
+        append_file "vendor/assets/javascripts/spree/backend/all.js", "//= require spree/backend/spree_blog\n"
+        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require spree/frontend/spree_blog\n"
       end
 
       def add_stylesheets
-        # inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_blog\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_blog\n", :before => /\*\//, :verbose => true
+        inject_into_file "vendor/assets/stylesheets/spree/backend/all.css", " *= require spree/backend/spree_blog\n",
+          :before => /\*\//, :verbose => true
+        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", " *= require spree/frontend/spree_blog\n",
+          :before => /\*\//, :verbose => true
       end
-
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_blog'
       end
